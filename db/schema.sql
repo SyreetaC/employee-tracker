bf0a4c8f60aca8ce09931234d29e401a2475496c
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-22 13:01:49 +0100

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,33 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-USE employees_db;
+USE company_db;
 
-(CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  surname VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NULL,
+CREATE TABLE departments(
+  id INT NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
-);)
+);
 
-(CREATE TABLE job_role (
+CREATE TABLE job_roles (
   id INT NOT NULL,
   title VARCHAR (30) NOT NULL,
   salary DECIMAL(10,2),
   department_id INT NOT NULL,
   PRIMARY KEY (id)
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
 
-);)
-
-(CREATE TABLE department(
-  id INT NOT NULL,
-  dept_name VARCHAR(30) NOT NULL,
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
-))
+   FOREIGN KEY (role_id) REFERENCES job_roles(id)
+   FOREIGN KEY (manager_id) REFERENCES employee(id) 
+);
+
+
+
