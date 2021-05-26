35369904fea58c12efdaac78f455ce3fbe33a746
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-26 05:42:39 +0100

    added methods to db connection

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ class Db {
   constructor(database) {
     const dbOptions = {
       host: "localhost",
-      port: 3306,
+      port: 3000,
       user: "root",
       password: "password",
       database,
@@ -13,6 +13,61 @@ class Db {
 
     this.database = database;
     this.connection = mysql.createConnection(dbOptions);
+  }
+
+  //starts the connection to the DB
+  start() {
+    return new Promise((resolve, reject) => {
+      const onConnect = (err) => {
+        if (err) reject(err);
+        console.log(
+          `Connection to ${this.database} database was successful with id ${this.connection.threadId}`
+        );
+        resolve();
+      };
+
+      this.connection.connect(onConnect);
+    });
+  }
+
+  end(message) {
+    this.connection.end();
+    console.log(
+      message ||
+        `Connection to ${this.database} database has been successfully closed.`
+    );
+  }
+
+  query(sqlQuery) {
+    return new Promise((resolve, reject) => {
+      const handleQuery = (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      };
+
+      this.connection.query(sqlQuery, handleQuery);
+    });
+  }
+
+  parameterisedQuery(sqlQuery, args, info = false) {
+    return new Promise((resolve, reject) => {
+      const handleQuery = (err, rows) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(rows);
+      };
+
+      const query = this.connection.query(sqlQuery, args, handleQuery);
+
+      if (info) {
+        console.log(query.sql);
+      }
+    });
   }
 }
 
