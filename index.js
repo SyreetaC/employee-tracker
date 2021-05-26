85e54a7a3dd9498acf589a36ec1712f9578ef8fa
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-26 05:25:36 +0100

    checked index js working with c.log

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
 //where logic for queries will be stored
+
+//give initial options using inquirer
+// view ...., create..., update...., delete...exit...
+
+//exit = connection end
+
+console.log("hello");
