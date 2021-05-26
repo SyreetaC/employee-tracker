e5cdccc6d0e1e6725dece65182428be56c5ed02b
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-26 06:14:07 +0100

    pseudo code added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const Db = require("./db/db");
 
+//research console table to present each query in a table
+
 //where logic for queries will be stored
 
 const init = async () => {
@@ -24,16 +26,44 @@ const initialOptions = async () => {
       "Create new Employee",
       "Add new Role",
       "Add new department",
-      "Update Employee",
-      "Update Role",
-      "Update Department",
+      "Update Employee details",
       "Exit",
     ],
   });
-
   //add switch/case here to go through each option. If exit, connection end.
 };
 
-//exit = connection end
+//function to view each employee, department and role.
+const viewEmployees = async () => {
+  console.log("View employees here");
+  //await connection to table
+  initialOptions();
+};
 
-init(initialOptions()); //db not currently connecting when this is called.
+//function to add new with questions from inquirer again. Once questions answered, insert into correct table using INSERT INTO
+const addEmployee = async () => {
+  console.log("add new employee here");
+  //await inquirer prompt answers
+  initialOptions();
+};
+
+const addRole = async () => {
+  console.log("add new role here");
+  //await inquirer prompt answers
+  initialOptions();
+};
+
+const addDepartment = async () => {
+  console.log("add new department here");
+  //await inquirer prompt answers
+  initialOptions();
+};
+
+//function to choose a specific employee by id and update it/ choose from a list of employees? UPDATE
+const updateEmployee = async () => {
+  console.log("update an employee here");
+  //await connection to table
+  //await inquirer prompt answers
+  initialOptions();
+};
+init();
