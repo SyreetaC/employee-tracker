0f9b490980d79f79c51d6a500b870230860addf4
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-26 05:42:21 +0100

    added init function to run at start

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Db = require("./db/db");
 
 //where logic for queries will be stored
 
+const init = async () => {
+  const db = new Db("company_db");
+
+  await db.start();
+};
+
 //give initial options using inquirer
 // view ...., create..., update...., delete...exit...
+const initialOptions = async () => {
+  let answer = await inquirer.prompt();
+};
 
 //exit = connection end
 
-console.log("hello");
+init();
