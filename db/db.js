db48f45bafb3631731fe4f3f71af2bc53d0ee8cf
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-26 06:02:24 +0100

    connection to db successful

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ class Db {
   constructor(database) {
     const dbOptions = {
       host: "localhost",
-      port: 3000,
+      port: 3306,
       user: "root",
       password: "password",
       database,
